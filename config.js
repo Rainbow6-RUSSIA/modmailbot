142b02300fc72e78fc445a1a1c6d845ab89cea3c
BadCoder1337 <anton.tiz.musin@gmail.com>
2019-08-07 22:22:09 +0300

    update cfg

--- config.js
+++ config.js
@@ -6,17 +6,29 @@ module.exports = {
   "port": process.env.PORT,
   "url": process.env.URL,
 
-  "attachmentStorage": 'discord',
   "attachmentStorageChannelId": process.env.ATTACHMENT_STORAGE_CHANNEL,
+  "categoryAutomation": {
+    "newThread": process.env.CATEGORY_ID,
+  },
+  "ignoreAccidentalThreads": process.env.IGNORE_ACCIDENTAL === 'true',
+  "mentionUserInThreadHeader": process.env.HEADER_MENTION === 'true',
   "relaySmallAttachmentsAsAttachments": process.env.RELAY_ATTACHMENTS_AS_ATTACHMENTS === 'true',
   "requiredAccountAge": parseInt(process.env.REQUIRED_ACCOUNT_AGE),
+  "requiredTimeOnServer": parseInt(process.env.REQUIRED_MEMBER_AGE),
   "typingProxy": process.env.TYPING_PROXY === 'true',
   "typingProxyReverse": process.env.TYPING_PROXY_REVERSE === 'true',
-  "useNicknames": process.env.USE_NICKNAMES === 'true',
-  "allowMove": process.env.ALLOW_MOVE === 'true',
-  "mentionUserInThreadHeader": process.env.HEADER_MENTION === 'true',
-  "newThreadCategoryId": process.env.CATEGORY_ID,
+  
+  "accountAgeDeniedMessage": `Ваш аккаунт был создан недавно, для защиты от спама вы пока не можете отправлять сообщения.\nЕсли вы впервые на сервере, ваш вопрос наверняка решится, если вы прочитаете инструкцию по регистрации ${process.env.REGISTRATION_GUIDE}.`,
+  "allowMove": true,
+  "attachmentStorage": 'discord',
+  "responseMessage": "Спасибо за обращение! Администрация скоро ответит. Во избежание недоразумений сообщения записываются.",
+  "rolesInThreadHeader": true,
+  "status": "ЛС сервера",
   "syncPermissionsOnMove": true,
+  "timeOnServerDeniedMessage": `Вы только что зашли на сервер, для защиты от спама вы пока не можете отправлять сообщения.\nЕсли вы впервые на сервере, ваш вопрос наверняка решится, если вы прочитаете инструкцию по регистрации ${process.env.REGISTRATION_GUIDE}.`,
+  "updateNotifications": false,
+  "useNicknames": true,
+  // "closeMessage": "Спасибо за обращение! Тред закрыт администратором. Для повторного обращения напишите снова."
 
   "knex": {
     client: "pg",
@@ -24,9 +36,4 @@ module.exports = {
     useNullAsDefault: true,
     searchPath: ["knex", "mailbot"]
   },
-
-  "accountAgeDeniedMessage": "Ваш аккаунт был создан недавно, для защиты от спама вы пока не можете отправлять сообщения.",
-  "status": "ЛС сервера",
-  "responseMessage": "Спасибо за обращение! Администрация скоро ответит. Во избежание недоразумений сообщения записываются.",
-  // "closeMessage": "Спасибо за обращение! Тред закрыт администратором. Для повторного обращения напишите снова."
 }
