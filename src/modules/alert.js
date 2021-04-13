16ad6dbe5d25b45a5f543b5c588a470084923de8
BadCoder1337 <anton.tiz.musin@gmail.com>
2021-04-13 22:53:28 +0300

    Fix alert

--- src/modules/alert.js
+++ src/modules/alert.js
@@ -1,10 +1,10 @@
 module.exports = ({ bot, knex, config, commands }) => {
   commands.addInboxThreadCommand("alert", "[opt:string]", async (msg, args, thread) => {
     if (args.opt && args.opt.startsWith("c")) {
-      await thread.setAlert(null);
+      await thread.removeAlert(null);
       await thread.postSystemMessage("Оповещение при новом сообщении отменено.");
     } else {
-      await thread.setAlert(msg.author.id);
+      await thread.addAlert(msg.author.id);
       await thread.postSystemMessage(`Упоминание ${msg.author.username}#${msg.author.discriminator} при появлении новых сообщений в треде.`);
     }
   });
